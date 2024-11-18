08459e3c28feb172988795a4e10421dd7d7c13b7
Matheus Guilherme <104092352+theuzao@users.noreply.github.com>
2024-11-18 08:38:36 -0300

    Create funcoes_ex10.c

---
+++ .github/workflows/Funções/funcoes_ex10.c
@@ -0,0 +1,22 @@
+// 1. Procedimento de Saudação
+// Crie um procedimento que receba um nome como parâmetro e imprima uma saudação personalizada.
+// Exemplo de uso:
+// Entrada: "Maria"
+// Saída: "Olá, Maria!"
+
+#include <stdio.h>
+
+// Procedimento que imprime o nnome
+void nomeProcedimento(char nomeUsuario[]) {
+	printf("Olá, %s\n", nomeUsuario); // Imprime a mensagem com o nome recebido
+}
+
+int main() {
+	
+	char nomeUsuario[15]; // Declara um array para armazenar o nome do usuário
+	scanf("%s", nomeUsuario); // Lê o nome 
+
+	nomeProcedimento(nomeUsuario); // Chama o procedimento para exibir a mensagem
+
+  return 0;
+}
