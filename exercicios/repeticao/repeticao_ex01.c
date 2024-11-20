bb3de46bc9e75033665e0786b01ef56a81af0d68
Matheus Guilherme <104092352+theuzao@users.noreply.github.com>
2024-11-20 00:15:50 -0300

    Create repeticao_ex01.c

---
+++ exercicios/repeticao/repeticao_ex01.c
@@ -0,0 +1,15 @@
+//1. Contar de 1 a 10
+//Escreva um programa que imprima os números de 1 a 10.
+
+#include <stdio.h>
+
+int main() {
+
+  // Laço para contar de 1 a 10
+  for (int i = 1; i <= 10; i++) {
+    printf("%d ", i); // Imprime o número atual seguido de um espaço
+  }
+
+  printf("\n");
+  return 0;
+}
