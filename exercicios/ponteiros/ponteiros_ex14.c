4de48ea2098b0ff7923851a249103bace682c48e
Matheus Guilherme <104092352+theuzao@users.noreply.github.com>
2024-12-09 07:05:01 -0300

    Create ponteiros_ex14.c

---
+++ exercicios/ponteiros/ponteiros_ex14.c
@@ -0,0 +1,23 @@
+//Operações em uma matriz usando ponteiros
+#include <stdio.h>
+
+int somaVetor(int *vet, int n) {
+    int soma = 0;
+    for (int i = 0; i < n; i++) {
+        soma += *(vet + i); // Soma usando o ponteiro
+    }
+    return soma;
+}
+
+int main() {
+    int vetor[5];
+    printf("Digite 5 números inteiros:\n");
+    for (int i = 0; i < 5; i++) {
+        scanf("%d", &vetor[i]);
+    }
+
+    int soma = somaVetor(vetor, 5);
+    printf("\nSoma dos elementos do vetor: %d\n", soma);
+
+    return 0;
+}
